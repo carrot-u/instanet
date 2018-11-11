af569b63b9b8d82b3995ed7afd6685a16b91d29c
Michael Prescott <Michael Prescott>
2018-11-11 14:50:08 -0800

    Adding Teams/Users test in test/system from scaffolds, also added scaffolds styling scss. Formatting may change for routes, but routes should remain the same.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  resources :welcome, only: [:index]  
+  resources :welcome, only: [:index]
   resources :teams do
     resources :users
   end
