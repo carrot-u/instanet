76b07b06715f379924e6d3c69e781d235986c014
Michael Prescott <Michael Prescott>
2018-11-12 14:55:35 -0800

    Adding initial User Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,18 @@ class User < ApplicationRecord
   has_many :users, class_name: 'User'
   belongs_to :team
   belongs_to :manager, class_name: 'User', optional: true
+
+  validates :first_name, presence: true
+  validates :first_name, length: { minimum: 2 }
+  validates :last_name, presence: true
+  validates :last_name, length: { minimum: 2 }
+  validates :title, presence: true
+  validates :title, length: { minimum: 2 }
+  validates :is_manager, presence: true
+  validates :active, presence: true
+
+  validates_associated :users
+  validates :manager, presence: true
+  validates :team, presence: true
+
 end
