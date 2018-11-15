eaf9ccd786272ad9cacc9f6b3bbfe389bdbd50cd
Michael Prescott <Michael Prescott>
2018-11-14 17:31:57 -0800

    Adding a Deactivate button to Users. Uses an update method in controller, a new route for Users, and a button to the route in the edit view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
   resources :welcome, only: [:index]
   resources :teams, :except => [:delete] do
-    resources :users, :except => [:delete]
+    resources :users, :except => [:delete] do
+      get '/deactivate', to: 'users#deactivate', as: 'deactivate'
+    end
   end
 end
