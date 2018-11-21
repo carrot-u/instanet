363e8e0393b77a0f00c965f0493222412027c348
Michael Prescott <Michael Prescott>
2018-11-20 19:04:25 -0800

    Sneakily fixing set_team for sneaky addition

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :set_user_deactivate, only: [:deactivate]
-  before_action :set_team, only: [:new]
+  before_action :set_team, only: [:new, :create]
 
   # GET /users
   # GET /users.json
