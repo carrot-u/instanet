9f56075ac9e1372b70ac9a81bdf66fdca9655343
Michael Prescott <Michael Prescott>
2018-12-13 14:46:27 -0800

    Resetting Syntax

--- db/migrate/20181101222459_add_user_to_teams.rb
+++ db/migrate/20181101222459_add_user_to_teams.rb
@@ -1,5 +1,5 @@
 class AddUserToTeams < ActiveRecord::Migration[5.2]
   def change
-    add_reference :teams, :manager, index: {to_table: :users}
+    add_reference :teams, :manager, index: true
   end
 end
--- db/migrate/20181205201714_create_user_skills.rb
+++ db/migrate/20181205201714_create_user_skills.rb
@@ -1,7 +1,7 @@
 class CreateUserSkills < ActiveRecord::Migration[5.2]
   def change
     create_table :user_skills do |t|
-      t.references :user, foreign_key: {to_table: :users}
+      t.references :user, foreign_key: true
       t.string :name
       t.integer :level
       t.boolean :active
