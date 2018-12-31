33ac93db717a094186a5d84ec44cea04252bd5a3
Michael Prescott <Michael Prescott>
2018-12-30 20:27:36 -0800

    Attemtping Heroku Fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,10 @@
 OmniAuth.config.full_host = Rails.env.production? ? 'https://morning-lake-67624.herokuapp.com/' : 'http://localhost:3000'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:client_secret]
+  provider :google_oauth2, Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:client_secret] {
+    scope: 'userinfo.email, userinfo.profile, plus.me',
+    include_granted_scopes: true,
+    callback_path: '/auth/google_oauth2/callback'
+  }
+end
 end
