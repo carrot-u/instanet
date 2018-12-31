1bfe4ed192d0bb9fc64430d7e2a4d10f130f71e8
Michael Prescott <Michael Prescott>
2018-12-30 20:44:06 -0800

    Attempting Heroku Fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-OmniAuth.config.full_host = Rails.env.production? ? 'https://morning-lake-67624.herokuapp.com/' : 'http://localhost:3000'
+OmniAuth.config.full_host = Rails.env.production? ? 'https://morning-lake-67624.herokuapp.com' : 'http://localhost:3000'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:client_secret]
